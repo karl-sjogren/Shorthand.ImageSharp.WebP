ce810e67994b4c45f78b787e52d46d5a04ace05f
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2020-12-20 14:01:19 +0100

    Updated test values

--- test/Shorthand.ImageSharp.WebP.Tests/WebPEncoderTests.cs
+++ test/Shorthand.ImageSharp.WebP.Tests/WebPEncoderTests.cs
@@ -9,27 +9,24 @@ namespace Shorthand.ImageSharp.WebP.Tests {
     public class WebPEncoderTests {
         [Fact]
         public void EncodeSimpleImage() {
-            using(var image = new Image<Rgba32>(20, 20)) {
-                image.Mutate(x => x.BackgroundColor(Rgba32.ParseHex("FF6347")));
-                using(var ms = new MemoryStream()) {
-                    image.Save(ms, new WebPEncoder());
+            using var image = new Image<Rgba32>(20, 20);
+            image.Mutate(x => x.BackgroundColor(Rgba32.ParseHex("FF6347")));
 
-                    Assert.True(ms.Length > 0, "Output stream should not be empty.");
-                    Assert.InRange(ms.Length, 60, 100);
-                }
-            }
+            using var ms = new MemoryStream();
+            image.Save(ms, new WebPEncoder());
+
+            Assert.True(ms.Length > 0, "Output stream should not be empty.");
+            Assert.InRange(ms.Length, 30, 50);
         }
 
         [Fact]
         public void EncodeFromFile() {
-            using(var image = Image.Load("Resources/test.jpg")) {
-                using(var ms = new MemoryStream()) {
-                    image.Save(ms, new WebPEncoder());
+            using var image = Image.Load("Resources/test.jpg");
+            using var ms = new MemoryStream();
+            image.Save(ms, new WebPEncoder());
 
-                    Assert.True(ms.Length > 0, "Output stream should not be empty.");
-                    Assert.InRange(ms.Length, 33000, 34000);
-                }
-            }
+            Assert.True(ms.Length > 0, "Output stream should not be empty.");
+            Assert.InRange(ms.Length, 205000, 206000);
         }
         [Fact]
         public async Task EncodeSimpleImageAsync() {
@@ -40,7 +37,7 @@ namespace Shorthand.ImageSharp.WebP.Tests {
             await image.SaveAsync(ms, new WebPEncoder());
 
             Assert.True(ms.Length > 0, "Output stream should not be empty.");
-            Assert.InRange(ms.Length, 60, 100);
+            Assert.InRange(ms.Length, 30, 50);
         }
 
         [Fact]
@@ -51,7 +48,7 @@ namespace Shorthand.ImageSharp.WebP.Tests {
             await image.SaveAsync(ms, new WebPEncoder());
 
             Assert.True(ms.Length > 0, "Output stream should not be empty.");
-            Assert.InRange(ms.Length, 33000, 34000);
+            Assert.InRange(ms.Length, 205000, 206000);
         }
 
         [Fact]
